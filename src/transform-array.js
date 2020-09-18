1eb2e348c6b515a11c0a0459c86506964110597c
ArtLavr <onemurder39@gmail.com>
2020-09-18 20:54:05 +0300

    done hanoi and chain

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!(arr instanceof Array)) throw Error("");
+  // throw new CustomError('Not implemented');
+
+
+  for (let index = 0; index < arr.length; index++) {
+    let currentValue = arr[index];
+    if (currentValue == "--discard-prev") {
+      if (index == 0) {
+        arr.splice(0, 1);
+        continue;
+      }
+      arr.splice(index - 1, 2);
+      index = index - 2;
+    } else if (currentValue == "--double-prev") {
+      if (index == 0) {
+        // console.log(arr);
+        arr.splice(0, 1);
+        // console.log(arr);
+        continue;
+      }
+      arr.splice(index, 1, arr[index - 1]);
+      index = index - 2;
+    } else if (currentValue === "--double-next") {
+      if (index == arr.length - 1) {
+        // console.log(arr);
+        arr.splice(arr.length - 1, 1);
+        // console.log(arr);
+        continue;
+      }
+      // console.log(arr);
+      arr.splice(index, 1, arr[index + 1]);
+      // console.log(arr);
+      index = index - 2;
+    } else if (currentValue == "--discard-next") {
+      if (index == arr.length - 1) {
+        // console.log(arr);
+        arr.splice(arr.length - 1, 1);
+        // console.log(arr);
+        continue;
+      }
+      // console.log(arr);
+      arr.splice(index, 2);
+      // console.log(arr);
+      index = index - 2;
+    }
+  }
+  return arr;
 };
